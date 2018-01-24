5d364b8499a0e76eb67cd215f3461f49933b3bb1
Dimage Sapelkin <podlec@gmail.com>
2018-01-24 14:41:46 +0100

    Added the electron-debug module

--- index.js
+++ index.js
@@ -10,9 +10,15 @@ const path = require("path");
   origin: 'https://www.adventurebox.com',
 });*/
 
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, Menu, MenuItem} = electron;
 
 let mainWindow;
+const menu = new Menu();
+menu.append(new MenuItem({
+  label: 'DevTools',
+  accelerator: "F12",
+  click: () => { mainWindow.webContents.toggleDevTools(); }
+}));
 
 app.on('browser-window-created',function(e,window) {
     window.setMenu(null);
@@ -30,4 +36,11 @@ app.on("ready", function(){
     protocol: "file:",
     slashes: true
   })*/ "https://www.adventurebox.com");
+  mainWindow.addEventListener("keydown", function (e) {
+		if (e.which === 123) {
+			mainWindow.webContents.toggleDevTools();
+		} else if (e.which === 116) {
+			location.reload();
+		}
+	}, true);
 });
